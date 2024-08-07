40ee31f89ece8c8902e7ffe3ba577fb044e7c01f
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-08-07 09:19:04 +0000

    [Add]Post_index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to root_path
+  end
+
+  #ログイン前のTopページ
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
