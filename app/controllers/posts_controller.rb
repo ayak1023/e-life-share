90c1ca9d81fcb46e04adee23812c02c6f2c2bd43
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-08-07 10:47:40 +0000

    [Add]post_delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,11 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to '/posts'
+  end
 
   private
   def post_params
