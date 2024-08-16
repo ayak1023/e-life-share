523e632bf3ce8d285c33ea166bd462a08773dcb1
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-08-16 13:27:18 +0000

    [Add]seed_file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "seedの実行を開始"
+
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.png"), filename:"sample-user1.png")
+end
+
+james = User.find_or_create_by!(email: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.png"), filename:"sample-user2.png")
+end
+
+lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.png"), filename:"sample-user3.png")
+end
+
+Post.find_or_create_by!(title: "早起きしやすくする方法") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.png"), filename:"sample-post1.png")
+  post.body = "起きる時間は徐々に早くしていき、1週間は続ける。寝る1時間前にはスマホから離れる。寝る3時間前に夕食は済ませておく。7時間は寝る。"
+  post.user = olivia
+end
+
+Post.find_or_create_by!(title: "1年使わなかったものは手放す") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.png"), filename:"sample-post2.png")
+  post.body = "快適な暮らしのために断捨離は必須です。１年間使わなかったものは、次の１年間も大体使わないので、潔く捨てましょう。"
+  post.user = james
+end
+
+Post.find_or_create_by!(title: "排水溝のぬめり対策") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.png"), filename:"sample-post3.png")
+  post.body = '排水溝のぬめりは丸めたアルミホイルを数個入れるだけで抑えられます。'
+  post.user = lucas
+end
+
+puts "seedの実行が完了しました"
