d20fc31dfbfc229db18d902cef3b7977a420f582
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-08-21 22:32:42 +0000

    [Add]コメント機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   has_many :posts, dependent: :destroy
-  #has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy
   #has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
 
+  resources :posts, only: [:new, :create, :index, :show, :destroy] do
+    resources :comments, only: [:create, :destroy]
+  end
+
 end
