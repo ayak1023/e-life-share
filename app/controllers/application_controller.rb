a3c62e36c4b2c260987ecb2edbb7c34ab04c168a
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-08-23 09:52:42 +0000

    [Add]管理者ログイン＆ユーザー一覧＆ユーザー削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top], unless: :admin_controller?
   before_action :authenticate_user!, except: [:top, :about, :index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,6 +9,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     about_path
+  end
+
+  private
+  def admin_controller?
+    self.class.module_parent_name == 'Admin'
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+
+
   devise_for :users
   root to: "homes#top"
   get 'about', to: 'homes#about', as: :about
