886d6569a3912ce817f7bc5b04e91a43652adc93
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-09-06 11:06:49 +0000

    [Add]post_new_page_layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,7 +54,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :body, :image)
+    params.require(:post).permit(:title, :body, :image, category_ids: [])
   end
 
   def is_matching_login_user
