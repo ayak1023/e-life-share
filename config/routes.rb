ff32ca1059dde7c6ce6ece9671c68e3a38946345
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-09-07 11:46:55 +0000

    [Add]タグ検索（リンク）機能

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,12 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+
   root to: "homes#top"
   get 'about', to: 'homes#about', as: :about
   get 'mypage', to: 'users#mypage', as: :mypage
   get "search" => "searches#search"
+  get 'categories/:id/posts', to: 'categories#posts', as: 'category_posts'
 
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     # ユーザーのフォロワー、フォロー関係
@@ -28,11 +30,9 @@ Rails.application.routes.draw do
     end
   end
 
-
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
-
 
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :comments, only: [:create, :destroy]
