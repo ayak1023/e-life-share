6cdfe7b57d7939edcfaa42fa195b8c73114f3451
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-09-17 22:00:42 +0000

    [Add]検索結果画面

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,28 +1,24 @@
 class Admin::DashboardsController < ApplicationController
-    layout 'admin'
-    before_action :authenticate_admin!
-    def index
-        @users = User.all
-    end
+  layout 'admin'
+  before_action :authenticate_admin!
 
-def search_result
-    # 検索条件を取得
-    range = params[:range] # ユーザー, 投稿, カテゴリから選択
+  def index
+    @users = User.all
+  end
+
+  def search_result
+    @range = params[:range]  # @rangeをビューで使えるようにインスタンス変数にする
     keyword = params[:word]
-    search_type = params[:search] # 部分一致、完全一致など
+    search_type = params[:search]
 
-    # 検索ロジックの実装
-    if range == 'User'
+    if @range == 'User'
       @results = search_users(keyword, search_type)
-    elsif range == 'Post'
-      @results = search_posts(keyword, search_type)
-    elsif range == 'Category'
+    elsif @range == 'Category'
       @results = search_categories(keyword, search_type)
     end
 
-    # 検索結果をビューで表示
-    render 'admin/dashboards/search_result'
-end
+    render 'admin/dashboards/search_result'  # search_resultビューをレンダリング
+  end
 
   private
 
