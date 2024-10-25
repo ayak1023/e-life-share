142c250521a7fbf1b8cd55f159405da132e9a03c
EC2 Default User <ec2-user@ip-172-31-23-255.ap-northeast-1.compute.internal>
2024-10-25 22:47:41 +0000

    [Add]Rspec_sessionControllers

---
+++ spec/controllers/session_controllers.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Public::CommentsController, type: :controller do
+
+  before do
+     @guest_user = create(:user, :guest)
+     allow(User).to receive(:guest).and_return(@guest_user)
+  end
+
+  describe "POST #guest_sign_in" do
+    it "ゲストユーザーでログインする" do
+      post :guest_sign_in
+      expect(controller.current_user).to eq(@guest_user)
+    end
+
+    it "mypage_path にリダイレクトし、通知を表示する" do
+      post :guest_sign_in
+      expect(response).to redirect_to(mypage_path)
+      expect(flash[:notice]).to eq("guestuserでログインしました。")
+    end
+  end
+
+end
